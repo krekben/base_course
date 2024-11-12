d9349b63813c0471757ba0bdff641f1b1667bd4f
krekben <145156783+krekben@users.noreply.github.com>
2024-11-12 15:35:48 +0000

    vfcffrf

---
+++ ooo.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("Маша + Петя = Любовь")
+print("x = 3 + 4")
+print(3 + 4)
+print("x = 7")
+a = [1, 5, "Good", "Bad"]
+b = [9, "Blue", "Red", 11]
+print(a[1] + b[3])
+print(a[2] + b[2])
+print(a[0] * b[0])
+print(a[1] ** b[3])
+print(a + b)
+
